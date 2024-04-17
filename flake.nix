0875be34e923329477d6077ec6dfdbcc49bf7807
MassimiIiano <massimiliano.mola.bzs@gmail.com>
2024-04-17 15:21:14 +0200

    hananas

--- flake.nix
+++ flake.nix
@@ -1,23 +1,16 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "nixpkgs/nixos-23.11";
-
-    home-manager-unstable.url = "github:nix-community/home-manager/master";
-    home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs";
-
-    home-manager-stable.url = "github:nix-community/home-manager/release-23.11";
-    home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
-  };
-
   outputs = { self, nixpkgs, ... }@inputs: 
   let 
     # configure system
-    systemSettings = {
+    system = {
       system = "x86_64-linux";
       profile = "laptop";
+    };
+
+    user = {
+      wm = "myprland";
     };
 
     # configure lib
@@ -30,11 +23,30 @@
     homeConfigurations = {};
     nixosConfigurations = {
       system = lib.nixosSystem {
-        system = systemSettings.system;
+        system = system.system;
         modules = [
-          ./profiles/laptop/configuration.nix
+          (./. + "/profiles" + ("/" + system.profile) + "/configuration.nix")        
         ];
       };
     };
   };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "nixpkgs/nixos-23.11";
+
+    home-manager-unstable.url = "github:nix-community/home-manager/master";
+    home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager-stable.url = "github:nix-community/home-manager/release-23.11";
+    home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
+
+    hyprland.url = "github:hyprwm/Hyprland";
+    plugin_name = {
+        url = "github:maintener/plugin_name";
+        inputs.hyprland.follows = "hyprland"; # IMPORTANT
+    };
+  
+  };
+
 }

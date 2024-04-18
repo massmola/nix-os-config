c9e18b45ebcd79139d6b54ea902482572eae6a13
assimiIiano <massimiliano.mola.bzs@gmail.com>
2024-04-18 16:16:23 +0200

    Commit on Thu 18 Apr 16:16:23 CEST 2024

---
+++ refresh.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Navigate to your repository directory
+cd ~/Documents/nix-configuration 
+
+# Add all files to the staging area
+git add .
+
+# Commit the changes with the current date and time as the message
+git commit -m "Commit on $(date)"
+
+# Push the changes to the remote repository
+git push
+
+# Rebuld NixOS
+# sudo nixos-rebuild switch --flake ./#system

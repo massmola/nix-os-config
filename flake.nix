4b9ec58ff5a2fcdce86180e56c702096c8390b01
assimiIiano <massimiliano.mola.bzs@gmail.com>
2024-04-18 16:40:46 +0200

    Commit on Thu 18 Apr 16:40:46 CEST 2024

--- flake.nix
+++ flake.nix
@@ -49,11 +49,5 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
-
-    home-manager-unstable.url = "github:nix-community/home-manager/master";
-    home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs";
-
-    home-manager-stable.url = "github:nix-community/home-manager/release-23.11";
-    home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
   };
 }

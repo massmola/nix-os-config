50234d2fa2f9fe73018ff0be0f92ddcccc465eb3
assimiIiano <massimiliano.mola.bzs@gmail.com>
2024-04-18 17:54:59 +0200

    Commit on Thu 18 Apr 17:54:59 CEST 2024

--- profiles/laptop/configuration.nix
+++ profiles/laptop/configuration.nix
@@ -22,7 +22,7 @@
     # nix flake
     nix.settings = {
         experimental-features = [ "nix-command" "flakes" ];
-            auto-optimise-store = true;
+        auto-optimise-store = true;
         substituters = [
             "https://hyprland.cachix.org"
             "https://nix-gaming.cachix.org"
@@ -32,6 +32,15 @@
             "https://nix-community.cachix.org"
             # Nix-community
             "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+            "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+            # Nixpkgs-Wayland
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+            # Nix-community
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         ];
     };
 

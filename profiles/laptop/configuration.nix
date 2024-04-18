aa6b9f701556611225e674ba735ce6197f80d7e7
assimiIiano <massimiliano.mola.bzs@gmail.com>
2024-04-18 18:04:12 +0200

    Commit on Thu 18 Apr 18:04:12 CEST 2024

--- profiles/laptop/configuration.nix
+++ profiles/laptop/configuration.nix
@@ -62,18 +62,25 @@
     time.timeZone = "Europe/Rome";
 
     # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
 
+    i18n.defaultLocale = "en_US.UTF-8";
     i18n.extraLocaleSettings = {
-        LC_ADDRESS = "en_GB.UTF-8";
-        LC_IDENTIFICATION = "en_GB.UTF-8";
-        LC_MEASUREMENT = "en_GB.UTF-8";
-        LC_MONETARY = "en_GB.UTF-8";
-        LC_NAME = "en_GB.UTF-8";
-        LC_NUMERIC = "en_GB.UTF-8";
-        LC_PAPER = "en_GB.UTF-8";
-        LC_TELEPHONE = "en_GB.UTF-8";
-        LC_TIME = "en_GB.UTF-8";
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+    };
+    i18n.inputMethod = {
+        enabled = "fcitx5";
+        fcitx5.addons = with pkgs; [
+            # fcitx5-unikey
+            fcitx5-with-addons
+        ];
     };
 
     services = {
@@ -82,15 +89,24 @@
             xkbVariant = "";
         };
         gvfs.enable = true;
+        xserver = {
+            enable = true;
+            libinput.enable = true; # touchpad support
+            displayManager.startx.enable = true;
+            desktopManager.gnome = {
+                enable = true;
+                extraGSettingsOverridePackages = [
+                pkgs.nautilus-open-any-terminal
+                ];
+            };
+        };
     };
-    # Configure keymap in X11
 
     # Enable CUPS to print documents.
     services.printing.enable = true;
     services.avahi.enable = true;
 
     # Enable touchpad support (enabled default in most desktopManager).
-    services.xserver.libinput.enable = true;
 
     # enable programms
 

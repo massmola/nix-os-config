7c4cfc7fd2007af80dc3c80e9a78d6df86576e32
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-04-20 00:45:11 +0200

    swotch: sab 20 apr 2024, 00:45:11, CEST

--- system/wayland/servicies.nix
+++ system/wayland/servicies.nix
@@ -1,16 +1,16 @@
 {pkgs, ...}: {
   systemd.services = {
-    # seatd = {
-    #   enable = true;
-    #   description = "Seat management daemon";
-    #   script = "${pkgs.seatd}/bin/seatd -g wheel";
-    #   serviceConfig = {
-    #     Type = "simple";
-    #     Restart = "always";
-    #     RestartSec = "1";
-    #   };
-    #   wantedBy = ["multi-user.target"];
-    # };
+    seatd = {
+      enable = true;
+      description = "Seat management daemon";
+      script = "${pkgs.seatd}/bin/seatd -g wheel";
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "1";
+      };
+      wantedBy = ["multi-user.target"];
+    };
   };
 
   services = {

a4b04bf548a7f6d2299a21a1c2000a63697a3112
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-04-19 13:59:26 +0200

    first marvin

--- hosts/marvin/hardware-configuration.nix
+++ hosts/marvin/hardware-configuration.nix
@@ -8,18 +8,18 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6babe1f3-1b5e-45c4-90db-6e09cb424e36";
+    { device = "/dev/disk/by-uuid/595ddc7f-da9b-4621-864e-7b70d2b44052";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D705-977E";
+    { device = "/dev/disk/by-uuid/1B41-9D44";
       fsType = "vfat";
     };
 

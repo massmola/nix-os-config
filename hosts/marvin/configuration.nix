4ea243efff14f3aecaa39a45bf98366d222cca38
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-04-19 23:22:58 +0200

    test nix: ven 19 apr 2024, 23:22:58, CEST

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -60,24 +60,12 @@
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${user.username} = {
-    isNormalUser = true;
-    description = user.description;
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [ ];
-  };
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     netcat
     curl
---
+++ system/core/users.nix
@@ -0,0 +1,10 @@
+{config, pkgs, user...}:
+{
+  programs.zsh.enable = true;
+  users.users.${user.username} = {
+    isNormalUser = true;
+    description = user.description;
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [   ];
+  };
+}

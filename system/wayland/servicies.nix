88fbfeccb35ebeef5f3e9f8109d90de4bb3ac9b5
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-04-20 11:47:20 +0200

    swotch: sab 20 apr 2024, 11:47:20, CEST

--- system/wayland/servicies.nix
+++ system/wayland/servicies.nix
@@ -1,16 +1,18 @@
-{pkgs, user, ...}: {
-  systemd.services = {
-    # seatd = {
-    #   enable = true;
-    #   description = "Seat management daemon";
-    #   script = "${pkgs.seatd}/bin/seatd -g wheel";
-    #   serviceConfig = {
-    #     Type = "simple";
-    #     Restart = "always";
-    #     RestartSec = "1";
-    #   };
-    #   wantedBy = ["multi-user.target"];
-    # };
+{pkgs, ...}: {
+  systemd.user.services = { 
+    polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
   };
 
   services = {

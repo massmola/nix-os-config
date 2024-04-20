ae53c22bb64f7053e75f2cfb5923e37029e94bca
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-04-20 03:11:35 +0200

    test nix: sab 20 apr 2024, 03:11:35, CEST

--- home/default.nix
+++ home/default.nix
@@ -8,11 +8,9 @@
   imports = [
     inputs.nix-index-db.hmModules.nix-index
     inputs.barbie.homeManagerModule
-    inputs.schizofox.homeManagerModule
-    inputs.impermanence.nixosModules.home-manager.impermanence
     inputs.hyprlock.homeManagerModules.default
     inputs.hypridle.homeManagerModules.default
-    
+
     ./packages.nix
     ./cli
     ./desktop

938d9b93e2974dbe37e486682f6098c278a8c15e
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-04-20 12:01:09 +0200

    test nix: sab 20 apr 2024, 12:01:09, CEST

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -19,18 +19,7 @@
   # Enable fakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
-  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
--- system/wayland/servicies.nix
+++ system/wayland/servicies.nix
@@ -16,6 +16,20 @@
   };
 
   services = {
+    xserver = { 
+      # Enable the X11 windowing system.
+      enable = true;
+
+      # Enable the GNOME Desktop Environment.
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+
+      # Configure keymap in X11
+      xkb.layout = "us";
+      xkb.variant = "";
+  
+     };
+
     gnome = {
       glib-networking.enable = true;
       gnome-keyring.enable = true;

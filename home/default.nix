d6f9cc47b242b31d4ef5941b08bb527ca9d1af51
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-04-20 03:08:18 +0200

    test nix: sab 20 apr 2024, 03:08:18, CEST

--- home/default.nix
+++ home/default.nix
@@ -6,6 +6,13 @@
 {
   config.home.stateVersion = "22.11";
   imports = [
+    inputs.nix-index-db.hmModules.nix-index
+    inputs.barbie.homeManagerModule
+    inputs.schizofox.homeManagerModule
+    inputs.impermanence.nixosModules.home-manager.impermanence
+    inputs.hyprlock.homeManagerModules.default
+    inputs.hypridle.homeManagerModules.default
+    
     ./packages.nix
     ./cli
     ./desktop
--- system/core/users.nix
+++ system/core/users.nix
@@ -4,7 +4,23 @@
   users.users.${user.username} = {
     isNormalUser = true;
     description = user.description;
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [
+      "wheel"
+      "gitea"
+      "docker"
+      "systemd-journal"
+      "vboxusers"
+      "audio"
+      "plugdev"
+      "wireshark"
+      "video"
+      "input"
+      "lp"
+      "networkmanager"
+      "power"
+      "nix"
+      "adbusers"
+    ];
     packages = with pkgs; [   ];
   };
 }

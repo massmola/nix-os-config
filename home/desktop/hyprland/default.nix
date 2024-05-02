d914cf9205e211284f88d2d861be540ae5f9f8d7
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-02 21:29:40 +0200

    test nix: gio 2 mag 2024, 21:29:40, CEST

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -1,5 +1,3 @@
-# FILEPATH: /home/spatola/Documents/nix/home/rice/hyprland/default.nix
-
 {
   pkgs,
   lib,
@@ -8,18 +6,16 @@
   ...
 }:
 with lib; let
-  # FILEPATH: /home/spatola/Documents/nix/home/rice/hyprland/default.nix
-
   # This code defines a function called `mkService` that creates a NixOS service.
   # The service is configured to be a part of the `graphical-session.target` unit,
   # and it should be started after the `graphical-session.target` unit is started.
   # Additionally, the service is set to be installed and wanted by the `graphical-session.target` unit.
 
-  mkService = lib.recursiveUpdate {
-    Unit.PartOf = ["graphical-session.target"];
-    Unit.After = ["graphical-session.target"];
-    Install.WantedBy = ["graphical-session.target"];
-  };
+  # mkService = lib.recursiveUpdate {
+  #   Unit.PartOf = ["graphical-session.target"];
+  #   Unit.After = ["graphical-session.target"];
+  #   Install.WantedBy = ["graphical-session.target"];
+  # };
 in {
   imports = [
     ./binds.nix 

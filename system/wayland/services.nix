9892d94a5c851d3d6ee175c9ed59e6477cdbd2f2
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-02 21:19:32 +0200

    test nix: gio 2 mag 2024, 21:19:32, CEST

--- system/wayland/services.nix
+++ system/wayland/services.nix
@@ -11,19 +11,18 @@
   };
 
   services = {
-    # xserver = { 
-    #   # Enable the X11 windowing system.
-    #   enable = true;
-    #   videoDrivers = [ "amdgpu" ];
+    xserver = { 
+      # Enable the X11 windowing system.
+      enable = true;
+      videoDrivers = [ "amdgpu" ];
 
-    #   # Enable the GNOME Desktop Environment.
-    #   # desktopManager.gnome.enable = true;
+      # Enable the GNOME Desktop Environment.
+      # desktopManager.gnome.enable = true;
 
-    #   # Configure keymap in X11
-    #   xkb.layout = "us";
-    #   xkb.variant = "";
-
-    # };
+      # Configure keymap in X11
+      xkb.layout = "us";
+      xkb.variant = "";
+    };
     gnome = {
       glib-networking.enable = true;
       gnome-keyring.enable = true;

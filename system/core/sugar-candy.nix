d76032c61c9ec41c0ce64e571ba022b09e8a495d
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-02 16:45:48 +0200

    test nix: gio 2 mag 2024, 16:45:48, CEST

--- system/core/sugar-candy.nix
+++ system/core/sugar-candy.nix
@@ -1,10 +1,10 @@
-{ stdenv, fetchFromGitHub, qt5 }:
+{ stdenv, fetchFromGitHub, qtbase }:
 {
   sddm-sugar-candy-theme = stdenv.mkDerivation rec {
     pname = "sddm-sugar-candy-theme";
     version = "1.6";
     dontBuild = true;
-    buildInputs = [ pt5.qtbase ];
+    buildInputs = [ qtbase ];
     installPhase = ''
       mkdir -p $out/share/sddm/themes
       cp -aR $src $out/share/sddm/themes/sugar-candy

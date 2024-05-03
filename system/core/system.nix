8d7982ce89379d8cc662a6de6c398b669252bb7a
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-04 01:25:20 +0200

    nixos switch: cleanup system.nix

--- system/core/system.nix
+++ system/core/system.nix
@@ -1,14 +1,6 @@
 {pkgs, lib, ...}: {
 
   services = {
-    # for desktop environments
-    dbus = {
-      packages = with pkgs; [dconf gcr udisks2];
-      enable = true;
-    };
-
-    udev.packages = with pkgs; [gnome.gnome-settings-daemon android-udev-rules];
-
     # collects messages from the kernel, early boot stages, and system processes, as well as any messages sent to syslog
     journald.extraConfig = ''
       SystemMaxUse=50M
@@ -17,20 +9,17 @@
 
     # manage disk drives and related devices
     udisks2.enable = true;
-
-    # profile-sync-daemon
-    psd = {
-      enable = true;
-      resyncTimer = "10m";
-    };
   };
 
+  # Enables the root slice for the Out-of-Memory Daemon (oomd).
   systemd = {
-    # Fedora enables these options by default. See the 10-oomd-* files here:
-    # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255
     oomd.enableRootSlice = true;
   };
 
+  # This configuration sets the garbage collector options for Nix.
+  # - `automatic` enables automatic garbage collection.
+  # - `dates` specifies the frequency of garbage collection, set to "daily".
+  # - `options` sets additional options for garbage collection, in this case "--delete-older-than 7d" which deletes files older than 7 days.
   nix = {
     gc = {
       automatic = true;
@@ -96,6 +85,7 @@
     # enable gamemode
     gamemode.enable = true;
   
+    # enables java
     java = {
       enable = true;
       package = pkgs.jre;
--- system/core/touchpad.nix
+++ system/core/touchpad.nix
@@ -4,5 +4,4 @@
   #   enable = true;
   #   touchpad.tapping = true;
   # };
-
 }

388f8f7164dd7a099019ca9e0830b7a2f651ab8d
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-04 01:06:50 +0200

    nixos switch: only git comment update

--- switch.sh
+++ switch.sh
@@ -4,10 +4,10 @@
 git add .
 
 # Commit the changes with the current date and time as the message
-git commit -m "nixos switch: $2"
+git commit -m "nixos switch: $1"
 
 # Push the changes to the remote repository
 git push
 
 # Rebuld NixOS
-sudo nixos-rebuild switch --flake ./#marvin --comment $1
+sudo nixos-rebuild switch --flake ./#marvin 

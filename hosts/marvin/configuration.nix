bf8c191f9b4f04c35dfba64b7b52f33af797ce5f
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-04 01:34:08 +0200

    nixos switch: better comments for host dir

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -1,20 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, user, ... }:
-
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
     ];
-
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable fakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];

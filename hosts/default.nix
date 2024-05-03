31958ea6f321b5cdd3792c822f9d2906ccef11a2
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-03 19:05:50 +0200

    swotch: ven 3 mag 2024, 19:05:50, CEST

--- hosts/default.nix
+++ hosts/default.nix
@@ -4,36 +4,10 @@
   user,
   ...
 }: let
-  # inherit (self) inputs;
-  # user info
-  # user = {
-  #   username = "spatola";
-  #   description = "Spatola";
-  # };
-  
   # system inports
   core = ../system/core;
   touchpad = ../system/core/touchpad.nix;
   wayland = ../system/wayland;
-  # hmModule = inputs.home-manager.nixosModules.home-manager;
-
-  # home manager
-  # home-manager = {
-  #   useUserPackages = true;
-  #   useGlobalPkgs = true;
-
-  #   extraSpecialArgs = {
-  #     inherit user;
-  #     inherit inputs;
-  #     inherit self;
-  #   };
-
-  #   users.${user.username} = {
-  #     imports = [../home];
-
-  #     _module.args.theme = import ../theme;
-  #   };
-  # };
 in {
 
   # laptop

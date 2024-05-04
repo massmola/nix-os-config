4cc0d7cf0df347c9fe8f9167d193b4078ebc5265
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-04 09:32:32 +0200

    nixos switch: added

--- system/core/system.nix
+++ system/core/system.nix
@@ -7,6 +7,17 @@
       RuntimeMaxUse=10M
     '';
 
+  openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
     # manage disk drives and related devices
     udisks2.enable = true;
   };

712a9e627870f2c053aa8e717949618bb325a5d5
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-04 14:15:58 +0200

    nixos switch:

--- system/wayland/pipewire.nix
+++ system/wayland/pipewire.nix
@@ -1,19 +1,14 @@
 {...}: {
-  # TODO: figure out why pipewire needs this line
+  sound.enable = true;
   hardware.pulseaudio.enable = false;
-
-  # rtkit is optional but recommended
   security.rtkit.enable = true;
-
-  # enable pipewire and its components
   services.pipewire = {
     enable = true;
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
-    wireplumber.enable = true;
+    audio.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.enable = true;
   };
 }

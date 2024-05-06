cef92a22fa0aef05c32b84af146953eb88cd6621
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-06 08:38:07 +0200

    nixos switch: added waydroid

--- system/core/docker.nix
+++ system/core/docker.nix
@@ -2,7 +2,14 @@
 {
 
   # Enable Docker virtualization
-  virtualisation.docker.enable = true;
+  virtualisation = {
+    # docker
+    docker.enable = true;
+
+    # waydroid
+    waydroid.enable = true;
+    lxd.enable = true;
+  }
 
   # Add the "docker" group to the user's extra groups
   users.users.${user.username}.extraGroups = [ "docker" ];

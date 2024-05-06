30f8db1a9de9ddc9871aaa941a6859e5499ef2c8
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-06 08:38:45 +0200

    nixos switch: fixed syntax error

--- system/core/docker.nix
+++ system/core/docker.nix
@@ -9,7 +9,7 @@
     # waydroid
     waydroid.enable = true;
     lxd.enable = true;
-  }
+  };
 
   # Add the "docker" group to the user's extra groups
   users.users.${user.username}.extraGroups = [ "docker" ];

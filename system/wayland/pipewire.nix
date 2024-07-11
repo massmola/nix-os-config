9313f0bbaa629c36cc77e1bd1422d785362da390
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-07-11 18:15:37 +0200

    switch marvin at gio 11 lug 2024, 18:15:37, CEST: try

--- system/wayland/pipewire.nix
+++ system/wayland/pipewire.nix
@@ -1,15 +1,10 @@
 {...}: {
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  hardware.enableAllFirmware  = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    audio.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
-    wireplumber.enable = true;
   };
 }

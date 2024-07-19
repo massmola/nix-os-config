06d6abb2c03b852bfe57b7b0ed17052d58529497
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-07-19 16:02:20 +0200

    chore: Rebuild NixOS and switch to flake #$1
    This commit rebuilds NixOS and switches to the flake #$1. It also includes the changes made in the recent user commits.

--- rebuild.sh
+++ rebuild.sh
@@ -4,14 +4,15 @@ set -e
 
 git pull
 
-sudo nixos-rebuild switch --flake ./#$1 
-
-# Rebuld NixOS
 gen=$(nixos-rebuild list-generations | grep current)
 # Add all files to the staging area
 git add -A
 # Commit the changes with the current date and time as the message
 git commit -m "switch $1: $2, $gen"
+
+sudo nixos-rebuild switch --flake ./#$1 
+
+# Rebuld NixOS
 
 # Push the changes to the remote repository
 git push

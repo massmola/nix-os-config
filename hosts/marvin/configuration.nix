b9ec8a7f9db13bf45169bd8ea74886fb795e4f75
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-08-05 17:31:39 +0200

    switch : , 125 current  2024-08-05 17:02:50  24.11.20240802.d049530  6.6.43                          *

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -150,6 +150,16 @@
     vlc 
   ];
 
+
+  virtualisation = {
+    # docker
+    docker.enable = true;
+
+    # waydroid
+    waydroid.enable = true;
+    lxd.enable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

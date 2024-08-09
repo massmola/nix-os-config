761994e5571610084a25c67e4b34412561d2c7ed
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-08-09 19:09:05 +0200

    switch marvin: renamed fields to suppress warnings, 128 current  2024-08-09 19:08:07  24.11.20240802.d049530  6.6.43                          *

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -51,13 +51,8 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
 
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;

e539b4d59eecdaadee8893e3dc8e5950d2bc15c1
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-08-09 19:13:00 +0200

    switch marvin: try to fix pulseaudio, 128 current  2024-08-09 19:08:07  24.11.20240802.d049530  6.6.43                          *

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -59,7 +59,7 @@
 
   # Enable sound with pipewire.
   
-  hardware.pulseaudio.enable = false;
+  # hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;

c9618654e48e697cf53e91f1436a166b4b7fb980
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-08-09 19:14:41 +0200

    switch marvin: try to fix pulseaudio, 128 current  2024-08-09 19:08:07  24.11.20240802.d049530  6.6.43                          *

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -59,7 +59,7 @@
 
   # Enable sound with pipewire.
   
-  # hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -67,11 +67,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).

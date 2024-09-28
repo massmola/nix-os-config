e9cddec57323d004945600f88081cec7fda564e7
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-09-28 14:50:13 +0200

    switch marvin: , 135 current  2024-09-28 14:37:24  24.11.20240910.1355a0c  6.6.50                          *

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -160,9 +160,9 @@
   # Install the driver
   services.fprintd.enable = true;
   # If simply enabling fprintd is not enough, try enabling fprintd.tod...
-  services.fprintd.tod.enable = true;
+  # services.fprintd.tod.enable = true;
   # ...and use one of the next four drivers
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
+  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
   # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-elan # Elan(04f3:0c4b) driver
   # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; # driver for 2016 ThinkPads
   # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a # Goodix 550a driver (from Lenovo)

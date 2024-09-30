61d7b5b1ab92636778963110aafe280cd30c1496
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-09-30 14:47:05 +0200

    switch marvin: mutter, 143 current  2024-09-30 14:39:01  24.11.20240910.1355a0c  6.6.50                          *

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -50,7 +50,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.desktopManager.gnome.sessionPath = [ mutter gnome-shell nautilus ]
+  services.xserver.desktopManager.gnome.sessionPath = [ mutter gnome-shell nautilus ];
 
 
   services.libinput.enable = true;

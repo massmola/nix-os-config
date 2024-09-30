36d6c0202f09b621eeceb3ab5830f81ae53492c1
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-09-30 14:38:28 +0200

    switch : , 142 current  2024-09-30 14:22:30  24.11.20240910.1355a0c  6.6.50                          *

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -48,8 +48,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  # services.displayManager.sddm.enable = true;
 
   # services.desktopManager.plasma6.enable = true;
   services.xserver.desktopManager.gnome.enable = true;

7797bee9f7eaccbf22f84f5109826c6c2391c3e3
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-09-30 14:22:05 +0200

    switch marvin: back to gnome, 141 current  2024-09-30 13:55:11  24.11.20240910.1355a0c  6.6.50                          *

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -51,8 +51,8 @@
   # services.xserver.displayManager.gdm.enable = true;
   services.displayManager.sddm.enable = true;
 
-  services.desktopManager.plasma6.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  # services.desktopManager.plasma6.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
 
   services.libinput.enable = true;

89d7b4e1925345ddaf1d7dcd3e173e47caed5c59
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-10-01 12:25:06 +0200

    switch marvin: fix fractional input, 145 current  2024-09-30 15:26:16  24.11.20240926.1925c60  6.6.52                          *

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -49,12 +49,8 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  services.xserver.desktopManager.gnome.experimentalFeatures = [
-    "scale-monitor-framebuffer"
-  ];
-
 
   services.libinput.enable = true;
 

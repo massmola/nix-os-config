c9f9c8e481b024f0495145de9cacc5611b7d85f2
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-10-04 19:07:58 +0200

    switch marvin: enable ssh, 166 current  2024-10-04 17:07:51  24.11.20241001.27e30d1  6.6.53                          *

--- hosts/marvin/configuration.nix
+++ hosts/marvin/configuration.nix
@@ -175,7 +175,17 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh ={
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = [ "spatola" ];
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  }
   services.flatpak.enable = true;
 
   systemd.services.flatpak-repo = {
@@ -188,7 +198,7 @@
 
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedTCPPorts = [ ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;

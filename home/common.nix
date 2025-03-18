474fd4657ea2147041829630df51346618fef3ef
MassimiIano <massimiliano.mola.bzs@gmail.com>
2025-03-18 15:00:26 +0100

    switch marvin: going to stable branch because ldx is not dooing its job attempt 2, 225 current  2025-03-17 11:04:06  25.05.20250315.c80f6a7  6.12.19                          *

--- home/common.nix
+++ home/common.nix
@@ -129,9 +129,9 @@
     '';
   };
 
-    programs.vscode = {
+  programs.vscode = {
     enable = true;
-    userSettings = {
+    profiles.default.userSettings = {
       "editor.fontFamily" = "'FiraCode Nerd Font', 'monospace'";
       "editor.fontLigatures" = true;
       "editor.fontSize" = 14;

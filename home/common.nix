5725cdb37fa527cfa980fbd2039799b2cb48c685
MassimiIano <massimiliano.mola.bzs@gmail.com>
2025-03-18 15:05:55 +0100

    switch marvin: fixing home manager missmatch, 225 current  2025-03-17 11:04:06  25.05.20250315.c80f6a7  6.12.19                          *

--- home/common.nix
+++ home/common.nix
@@ -146,7 +146,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "25.05";
+  home.stateVersion = "24.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;

fdde89116ade1b58ee4545a1f3978176b7763410
MassimiIano <massimiliano.mola.bzs@gmail.com>
2025-03-19 14:04:08 +0100

    switch marvin: changing home manager ver, 228 current  2025-03-19 14:01:53  25.05.20250318.b6eaf97  6.12.19                          *

--- flake.nix
+++ flake.nix
@@ -1,23 +1,30 @@
 {
   description = "spatola's config";
 
-  outputs = { self, nixpkgs-stable, nixpkgs, ... }@inputs: {
-    
-    # NixOS Configuration
-    nixosConfigurations.marvin = nixpkgs.lib.nixosSystem {
-      
-      system = "x86_64-linux";
+  outputs =
+    {
+      self,
+      nixpkgs-stable,
+      nixpkgs,
+      ...
+    }@inputs:
+    {
 
-      modules =
-        [
+      # NixOS Configuration
+      nixosConfigurations.marvin = nixpkgs.lib.nixosSystem {
+
+        system = "x86_64-linux";
+
+        modules = [
           # Name of the host
-          {networking.hostName = "marvin";}
+          { networking.hostName = "marvin"; }
 
           # Main configuration file for NixOS
           ./conf/configuration.nix
 
           # User configuration with home-manager
-          inputs.home-manager.nixosModules.home-manager {
+          inputs.home-manager.nixosModules.home-manager
+          {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
@@ -31,20 +38,25 @@
             # arguments to home.nix
           }
         ];
-      
-      specialArgs = { inherit inputs; };
+
+        specialArgs = { inherit inputs; };
+      };
     };
-  };
 
   inputs = {
 
     # Stable Nixpkgs
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11"; 
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     # Unstable Nixpkgs
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
-    
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager-stable = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -58,6 +70,6 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    
+
   };
 }

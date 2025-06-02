344abd1658935c53a5447344e5f0b7ea6281e961
MassimiIano <massimiliano.mola.bzs@gmail.com>
2025-06-02 16:05:07 +0200

    switch marvin: adjusten nainming conventions, 249 current  2025-06-02 16:02:52  25.11.20250531.910796c  6.12.31                          *

--- conf/configuration.nix
+++ conf/configuration.nix
@@ -42,9 +42,11 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager = {
+    gnome.enable = true;
+    gdm.wayland = true;
+    gdm.enable = true;
+  };
 
   services.libinput.enable = true;
 
